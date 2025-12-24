aa1d0f116cd3872117ce4b459e94a783a1522dbc
Abhay-us <choudharyabhay268@gmail.com>
2025-12-24 18:37:42 +0530

    JavaScript_added

---
+++ JavaScript/script.js
@@ -0,0 +1,80 @@
+// var a =12;
+// document.write(typeof(a)); 
+// document.write("<br>");
+// document.write(`<img src="" alt="Image test"><br>`);
+// document.write(a);
+
+// Single Line Comment
+/*
+MULTI LINE COMMENT
+*/
+
+// Conditions
+// // if-else statement
+// if (condition) {
+    
+// } else {
+    
+// }
+
+// // if-else-if statement
+// if (condition) {
+    
+// } else if (condition) {
+    
+// } else {
+
+// }
+
+// // Nexted If-else statement
+// if (condition) {
+//     if (condition) {
+        
+//     } else {
+        
+//     }
+// } else {
+//     if (condition) {
+        
+//     } else {
+        
+//     }
+// }
+
+// Iterative Statement (Loop)
+// for while do-while
+
+// // for statement
+// for (let index = 0; index < range; index++) {
+//     statement
+// }
+
+// // while statement
+// let index = 0;
+// while ( index < range) {
+//     statement;
+//     index++;
+// }
+
+// // do-while statement
+// let index = 0;
+// do {
+//     statement;
+//     index++;
+// } while ( index < range);
+
+
+// Even or Odd Number 
+// var a = 15;
+// if( a%2==0){
+//     document.writeln("Even number");
+// }else{
+//     document.writeln("Odd Number");
+// }
+
+
+// for(let i=1; i<=15;i++){
+//     if(i%2==0){
+//         document.writeln("<br>"+i);
+//     }
+// }
