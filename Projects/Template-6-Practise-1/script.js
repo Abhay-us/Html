c222f1af57fd35e1272b75f67f90ad420b77e5ae
Abhay-us <choudharyabhay268@gmail.com>
2026-01-29 07:56:22 +0530

    final-changes-in-template

--- Projects/Template-6-Practise-1/script.js
+++ Projects/Template-6-Practise-1/script.js
@@ -48,5 +48,7 @@ $(document).ready(function () {
   $(".menu-select").on("click", "li", function () {
     var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
+    $(".menu-select li").removeClass("active");
+    $(this).addClass("active");
   });
 });
